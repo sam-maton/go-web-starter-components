bdfa1303fb3c5bd85d62e0f37f442cadf40ee13e
Sam Maton <sam.maton91@gmail.com>
2026-06-05 10:50:14 +0100

    reset

--- main.go
+++ main.go
@@ -1,56 +1,28 @@
 package main
 
 import (
+	"fmt"
+	"html/template"
 	"log"
 	"net/http"
 )
 
-func newHandler() (http.Handler, error) {
-	templateCache, err := newTemplateCahce()
-	if err != nil {
-		return nil, err
-	}
-
-	mux := http.NewServeMux()
-
-	render := func(w http.ResponseWriter, name string) {
-		t, ok := templateCache[name]
-		if !ok {
-			http.Error(w, "template not found", http.StatusInternalServerError)
-			return
-		}
-
-		if err := t.ExecuteTemplate(w, "base", nil); err != nil {
-			http.Error(w, "failed to render template", http.StatusInternalServerError)
-		}
-	}
-
-	mux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		if r.URL.Path != "/" {
-			http.NotFound(w, r)
-			return
-		}
-
-		render(w, "home.html")
-	})
-
-	mux.HandleFunc("/about", func(w http.ResponseWriter, r *http.Request) {
-		render(w, "about.html")
-	})
-
-	mux.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("static"))))
-
-	return mux, nil
-}
-
 func main() {
-	handler, err := newHandler()
-	if err != nil {
-		log.Fatalf("failed to initialize handler: %v", err)
-	}
 
-	log.Println("server listening on http://localhost:8080")
-	if err := http.ListenAndServe(":8080", handler); err != nil {
-		log.Fatal(err)
-	}
+	http.Handle("GET /static/", http.StripPrefix("/static", http.FileServer(http.Dir("./ui/static/"))))
+
+	http.Handle("/", http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		t, err := template.ParseFiles("ui/html/pages/home.html")
+		if err != nil {
+			w.Write([]byte(err.Error()))
+		}
+
+		err = t.Execute(w, nil)
+		if err != nil {
+			w.Write([]byte(err.Error()))
+		}
+	}))
+
+	fmt.Println("Server started @ http://localhost:4321")
+	log.Fatal(http.ListenAndServe(":4321", nil))
 }
