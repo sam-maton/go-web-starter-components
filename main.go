5d510d74190ed0563eb30851a002a1e8415c088c
Sam Maton <sam.maton91@gmail.com>
2026-06-08 21:13:54 +0100

    added partials

--- main.go
+++ main.go
@@ -12,7 +12,11 @@ func main() {
 	http.Handle("GET /static/", http.StripPrefix("/static", http.FileServer(http.Dir("./ui/static/"))))
 
 	http.Handle("/", http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		t, err := template.ParseFiles("ui/html/base.html", "ui/html/pages/home.html")
+		render("home.html", w, r)
+	}))
+
+	http.Handle("/documentation", http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		t, err := template.ParseFiles("ui/html/base.html", "ui/html/pages/documentation.html", "ui/html/partials/topNav.html")
 		if err != nil {
 			w.Write([]byte(err.Error()))
 		}
--- utils.go
+++ utils.go
@@ -1,1 +1,18 @@
 package main
+
+import (
+	"html/template"
+	"net/http"
+)
+
+func render(page string, w http.ResponseWriter, r *http.Request) {
+	t, err := template.ParseFiles("ui/html/base.html", "ui/html/pages/"+page, "ui/html/partials/topNav.html")
+	if err != nil {
+		w.Write([]byte(err.Error()))
+	}
+
+	err = t.Execute(w, nil)
+	if err != nil {
+		w.Write([]byte(err.Error()))
+	}
+}
