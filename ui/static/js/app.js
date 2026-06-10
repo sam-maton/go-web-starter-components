5273495113f2c7bfb2ff9ef4959eaa0de9e02838
Sam Maton <sam.maton91@gmail.com>
2026-06-10 13:16:45 +0100

    finished dark mode, added link styles;

--- ui/static/js/app.js
+++ ui/static/js/app.js
@@ -1,1 +1,17 @@
-console.log('Javascript running...');
+const themeToggle = document.querySelector("#theme-toggle");
+themeToggle.addEventListener("click", () => {
+  const root = document.documentElement;
+  const currentTheme = root.getAttribute("data-theme");
+  let next = null;
+  if (currentTheme) {
+    next = currentTheme === "light" ? "dark" : "light";
+    root.setAttribute("data-theme", next);
+    localStorage.setItem("theme", next);
+    return;
+  }
+
+  const prefersDark = window.matchMedia("(prefers-color-scheme: dark)").matches;
+  next = prefersDark ? "light" : "dark";
+  root.setAttribute("data-theme", next);
+  localStorage.setItem("theme", next);
+});
