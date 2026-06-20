e044cbc8f2a9594df06a9b017dfa36b16c48fa3b
Sam Maton <sam.maton91@gmail.com>
2026-06-20 21:17:56 +0100

    setup blog fetch

--- main.go
+++ main.go
@@ -18,6 +18,11 @@ func main() {
 		log.Fatal(err)
 	}
 
+	pages, err := blogPages()
+	if err != nil {
+		log.Fatal(err)
+	}
+
 	app := Application{
 		cache: templateCache,
 	}
@@ -25,20 +30,20 @@ func main() {
 	http.Handle("GET /static/", http.StripPrefix("/static", http.FileServer(http.Dir("./ui/static/"))))
 
 	http.Handle("GET /", http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		app.render("home.html", w, r)
+		app.render("home.html", w, r, pages)
 	}))
 
 	http.Handle("GET /blog/{slug}", http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		slug := r.PathValue("slug")
-		app.render(fmt.Sprintf("%s.html", slug), w, r)
+		app.render(fmt.Sprintf("%s.html", slug), w, r, nil)
 	}))
 
 	http.Handle("GET /components", http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		app.render("components.html", w, r)
+		app.render("components.html", w, r, nil)
 	}))
 
 	http.Handle("GET /documentation", http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		app.render("documentation.html", w, r)
+		app.render("documentation.html", w, r, nil)
 	}))
 
 	fmt.Println("Server started @ http://localhost:4321")
--- utils.go
+++ utils.go
@@ -1,20 +1,84 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net/http"
+	"os"
+	"path/filepath"
+	"strings"
+	"time"
 )
 
-func (app *Application) render(page string, w http.ResponseWriter, r *http.Request) {
+type Metadata struct {
+	Title string
+	Date  time.Time
+}
+
+func (app *Application) render(page string, w http.ResponseWriter, r *http.Request, data interface{}) {
 	t, ok := app.cache[page]
 	if !ok {
 		http.Error(w, "Page not in cache", 500)
 	}
 
-	err := t.Execute(w, nil)
+	err := t.Execute(w, data)
 	if err != nil {
 		log.Println(err)
 		http.Error(w, "Cannot execute template", 500)
 	}
 
+}
+
+func blogPages() ([]Metadata, error) {
+	pages := []Metadata{}
+
+	paths, err := filepath.Glob("./ui/html/blog/*.html")
+	if err != nil {
+		return nil, err
+	}
+
+	for _, path := range paths {
+		data, err := os.ReadFile(path)
+		if err != nil {
+			fmt.Println(err)
+		}
+
+		pageMetadata, err := parseMetadata(string(data))
+
+		fmt.Println(pageMetadata)
+	}
+
+	return pages, nil
+}
+
+func parseMetadata(content string) (Metadata, error) {
+
+	var metadata Metadata
+
+	metaStart := strings.Index(content, "<!--")
+	metaEnd := strings.Index(content, "-->")
+
+	block := strings.TrimSpace(content[metaStart+4 : metaEnd])
+
+	for l := range strings.SplitSeq(block, "\n") {
+		l = strings.TrimSpace(l)
+
+		parts := strings.SplitN(l, ":", 2)
+
+		key := parts[0]
+		value := parts[1]
+
+		switch strings.ToLower(key) {
+		case "title":
+			metadata.Title = value
+		case "date":
+			date, err := time.Parse("2006-01-02", value)
+			if err != nil {
+				return metadata, err
+			}
+			metadata.Date = date
+		}
+	}
+
+	return metadata, nil
 }
